6341a0eca175622651d49a235740017b99a6ba49
Lewis Bock <lewis.bock@gmail.com>
2025-10-29 14:17:01 +0100

    initialisation projet avec structure de base

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+// src/App.jsx
+export default function App() {
+  return (
+    <main style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Argent Bank</h1>
+      <p>Bienvenue sur ton projet React 🧩</p>
+    </main>
+  );
+}
